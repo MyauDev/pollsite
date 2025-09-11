6cb54a699eefca4a1bf050960c0f4b924f2d2e3d
Артемий Мурин <a.muryn@libersave.com>
2025-09-11 19:09:51 +0200

    fix crud 2

--- api/polls/views_crud.py
+++ api/polls/views_crud.py
@@ -6,23 +6,18 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Poll
 from .serializers_write import PollWriteSerializer
 from .permissions import IsAuthorOrReadOnly
-
-# В settings.DEFAULT_AUTHENTICATION_CLASSES уже подключён CookieJWTAuthentication.
-# Ничего не переопределяем, чтобы работала авторизация по httpOnly cookie.
-
 class PollCreateView(generics.CreateAPIView):
     serializer_class = PollWriteSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Привязываем автора к текущему пользователю. Игнорируем author из входа.
         serializer.save(author=self.request.user)
 
 class PollUpdateView(generics.RetrieveUpdateAPIView):
     serializer_class = PollWriteSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     queryset = Poll.objects.all().select_related('author')
-    lookup_field = 'id'  # т.к. в urls используем <int:id>
+    lookup_field = 'id'
 
 class PollDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
